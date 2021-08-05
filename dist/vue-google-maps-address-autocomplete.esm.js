58d284f045029c9331b50db34a8fed5c90780a63
Thomas Sohet <sohet.thomas@gmail.com>
2021-08-05 15:47:00 +0200

    build iife, es, cjs format

---
+++ dist/vue-google-maps-address-autocomplete.esm.js
@@ -0,0 +1,103 @@
+var script = {
+  data () {
+    return {
+      addressAutocompleteRef: 'addressAutocompleteReference',
+    };
+  },
+  methods: {
+    loadScript (src, id = null) {
+      return new Promise(function (resolve, reject) {
+        if (document.querySelector('script[src="' + src + '"]')) {
+          resolve();
+
+          return;
+        }
+
+        const el = document.createElement('script');
+
+        el.type = 'text/javascript';
+        el.async = true;
+        el.src = src;
+        el.id = id;
+
+        el.addEventListener('load', resolve);
+        el.addEventListener('error', reject);
+        el.addEventListener('abort', reject);
+
+        document.head.appendChild(el);
+      });
+    },
+    loadGoogleMapsScript () {
+      this.loadScript(`https://maps.googleapis.com/maps/api/js?key=${this.$googleMapsApiKey}&libraries=places`)
+      .then(() => {
+        this.initializeAddressAutocomplete();
+      });
+    },
+    initializeAddressAutocomplete () {
+      const inputElement = this.$scopedSlots.default()[0].context.$refs[this.addressAutocompleteRef];
+
+      this.autocomplete = new google.maps.places.Autocomplete(
+        inputElement,
+        { types: ['geocode'] },
+      );
+
+      this.autocomplete.setComponentRestrictions({ country: ['be'] });
+
+      this.autocomplete.addListener('place_changed', () => {
+        const place = this.autocomplete.getPlace();
+        const ac = place.address_components;
+
+        const {
+          route: streetName,
+          street_number: streetNumber = null,
+          postal_code: zipCode = null,
+          locality: city = null,
+        } = ac.reduce(function (map, obj) {
+          map[obj.types[0]] = obj.short_name;
+          return map;
+        }, {});
+
+        this.$emit('udpateAddress', { streetName, streetNumber, zipCode, city });
+      });
+    },
+  },
+  render () {
+    return this.$scopedSlots.default({
+      loadGoogleMapsScript: this.loadGoogleMapsScript,
+      addressAutocompleteRef: this.addressAutocompleteRef,
+    });
+  },
+};
+
+script.__file = "src/AddressAutocomplete.vue";
+
+// Import vue component
+
+// install function executed by Vue.use()
+function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component('AddressAutocomplete', script);
+}
+// Create module definition for Vue.use()
+const plugin = {
+  install,
+};
+
+// To auto-install when vue is found
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+// Inject install function into component - allows component
+// to be registered via Vue.use() as well as Vue.component()
+script.install = install;
+
+export { script as default };
+//# sourceMappingURL=vue-google-maps-address-autocomplete.esm.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -19,5 +19,26 @@ export default [
 			commonjs(), // so Rollup can convert `ms` to an ES module
       vuePlugin()
 		]
+	},
+	// CommonJS (for Node) and ES module (for bundlers) build.
+	// (We could have three entries in the configuration array
+	// instead of two, but it's quicker to generate multiple
+	// builds from a single configuration where possible, using
+	// an array for the `output` option, where we can specify
+	// `file` and `format` for each target)
+	{
+		input: 'src/index.js',
+		external: ['ms'],
+		output: [
+			{ name: 'vueGooglemapsAddressAutocomplete', file: pkg.main, format: 'cjs', sourcemap: true },
+			{ name: 'vueGooglemapsAddressAutocomplete', file: pkg.module, format: 'es', sourcemap: true },
+			{ name: 'vueGooglemapsAddressAutocomplete', file: pkg.unpkg, format: 'iife', sourcemap: true }
+		],
+		plugins: [
+      peerDepsExternal(),
+			resolve(), // so Rollup can find `ms`
+			commonjs(), // so Rollup can convert `ms` to an ES module
+      vuePlugin()
+		]
 	}
 ];
